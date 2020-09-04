db668a314ac268636533aac2d553fd407b01e177
ckmonish2000 <ckmonish2000@gmail.com>
2020-09-04 16:50:50 +0530

    db init

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ var getRoute = require("./routes/get");
 
 var app = express();
 
+var mongoServer = "127.0.0.1:27017";
+var db = "issues";
+
+mongoose
+  .connect(`mongodb://${mongoServer}/${db}`)
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 app.use("/get", getRoute);
 
 app.listen(3000, () => {
